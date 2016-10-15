a00149fadfccb84aa6a08b29d9a6067e57fa0f4f
Ícaro Lima Magalhães <icaro.lmag@gmail.com>
2016-10-15 15:17:58 -0300

    Final greedy build before merge

--- modules/greedy/cppunit.cpp
+++ modules/greedy/cppunit.cpp
@@ -6,6 +6,23 @@ using namespace compendium;
 int main()
 {
 
+    int V, E;
+    int v, w; double weight;
+    std::cin >> V >> E;
+    EdgeWeightedGraph graph(V);
+
+    while(E--)
+    {
+        std::cin >> v >> w >> weight;
+        graph.add_edge(new Edge(v, w, weight));
+    }
+
+    KruskalMST kruskal_mst(graph);
+    kruskal_mst.get_result();
+
+    PrimMST prim_mst(graph);
+    prim_mst.get_result();
 
     return 0;
+
 }
